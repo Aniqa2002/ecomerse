6cc896f67ce069d74479272c966478baf80ac9e2
Aniqa Khalid <139966166+Aniqa2002@users.noreply.github.com>
2024-08-26 05:34:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidemenu = document.getElementById("sidemenu")
+    function openmenu(){
+        sidemenu.style.right= "-17%";
+    }
+    function closemenu(){
+        sidemenu.style.right= "-500px";
+    }
+
+
+    
